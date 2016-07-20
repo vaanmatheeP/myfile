b7b4c246e21c1e81843232879cbeb94eac66be45
vaanmatheeP <vaanmatheeairhosterss@gmail.com>
2016-07-20 10:17:15 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,17 @@
+package guvi2;
+
+import java.util.Scanner;
+
+public class positive {
+	public static void main(String ar[]){
+int n;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the number");
+n=s.nextInt();
+if(n%2==0){
+	System.out.println("no is positive");}
+	else{
+		System.out.println("no is negative");
+}
+}
+}
